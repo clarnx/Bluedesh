2962fc581fdfd722a625119edf83ad97b812cc2b
Ahtsham <74046267+Ahtsham110@users.noreply.github.com>
2022-09-23 15:02:09 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+        extend: {
+          colors: {
+            primary: '#58B0EE',
+            secondary: '#01C8FB',
+            tertiary: '#EFFBFF'
+          }
+
+},
+  },
+  plugins: [],
+}
